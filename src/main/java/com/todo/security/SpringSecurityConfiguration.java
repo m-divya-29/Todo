b40d70a6adc9dda54b2c79c7a65e692311813131
m-divya-29 <divyamahankali29@gmail.com>
2022-11-24 12:55:46 +0530

    Add new user.

--- src/main/java/com/todo/security/SpringSecurityConfiguration.java
+++ src/main/java/com/todo/security/SpringSecurityConfiguration.java
@@ -16,13 +16,19 @@ public class SpringSecurityConfiguration {
      */
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager(){
+        UserDetails user1 = createNewUser("Divya", "password");
+        UserDetails user2 = createNewUser("Vikas", "vikas123");
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
         UserDetails userDetails = User.builder()
                 .passwordEncoder(input -> passwordEncoder().encode(input)) //encode input password.
-                .username("Divya")
-                .password("password")
+                .username(username)
+                .password(password)
                 .roles("USER", "ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     /**
