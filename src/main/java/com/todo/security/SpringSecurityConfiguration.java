b1e5b0fc609e501ad8c7f19a1fab193b16d058ac
m-divya-29 <divyamahankali29@gmail.com>
2022-11-24 11:32:52 +0530

    BCrypt password encoder.

--- src/main/java/com/todo/security/SpringSecurityConfiguration.java
+++ src/main/java/com/todo/security/SpringSecurityConfiguration.java
@@ -4,18 +4,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SpringSecurityConfiguration {
+    /**
+     * Create custom user and encode password
+     * @return inmemoryuserdetailsmanager
+     */
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager(){
-        //create a user with default password encoding and return a manager for it.
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(input -> passwordEncoder().encode(input)) //encode input password.
                 .username("Divya")
                 .password("password")
                 .roles("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    /**
+     * Using BCryptPasswordEncoder, spring encodes and checks if the entered password matches.
+     * @return passwordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
